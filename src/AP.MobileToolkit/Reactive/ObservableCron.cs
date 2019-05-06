c0e8cd97aed05e0e14e0d4597f1b3ae19c24328b
Dan Siegel <me@dansiegel.net>
2019-05-05 21:06:13 -0700

    adding ObservableCron

---
+++ src/AP.MobileToolkit/Reactive/ObservableCron.cs
@@ -0,0 +1,20 @@
+﻿using NCrontab;
+using ReactiveUI;
+using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+namespace AP.MobileToolkit.Reactive
+{
+    public static class ObservableCron
+    {
+        public static IObservable<int> Cron(string cron) => Cron(cron, RxApp.TaskpoolScheduler);
+
+        public static IObservable<int> Cron(string cron, IScheduler scheduler)
+        {
+            var schedule = CrontabSchedule.Parse(cron);
+            return Observable.Generate(0, d => true, d => d + 1, d => d,
+                d => new DateTimeOffset(schedule.GetNextOccurrence(scheduler.Now.DateTime)), scheduler);
+        }
+    }
+}
