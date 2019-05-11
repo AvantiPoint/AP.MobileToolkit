5a5209ace7c9db9c819c004313bbffee0630f71f
Dan Siegel <me@dansiegel.net>
2019-05-11 14:47:37 -0700

    adding GravatarImageSource

---
+++ src/AP.MobileToolkit/Controls/Platform/Droid/GravatarImageSourceHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Graphics;
+using AP.MobileToolkit.Controls;
+using AP.MobileToolkit.Controls.Platform.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportImageSourceHandler(typeof(GravatarImageSource), typeof(GravatarImageSourceHandler))]
+namespace AP.MobileToolkit.Controls.Platform.Droid
+{
+    public class GravatarImageSourceHandler : IImageSourceHandler
+    {
+        public async Task<Bitmap> LoadImageAsync(ImageSource imagesource, Context context, CancellationToken cancelationToken = default)
+        {
+            if (imagesource is GravatarImageSource gis)
+            {
+                var imageBytes = await gis.GetGravatarAsync();
+
+                if (imageBytes.Length > 0)
+                {
+                    return BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/AP.MobileToolkit/Controls/Platform/iOS/GravatarImageSourceHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AP.MobileToolkit.Controls;
+using AP.MobileToolkit.Controls.Platform.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportImageSourceHandler(typeof(GravatarImageSource), typeof(GravatarImageSourceHandler))]
+namespace AP.MobileToolkit.Controls.Platform.iOS
+{
+    public class GravatarImageSourceHandler : IImageSourceHandler
+    {
+        public async Task<UIImage> LoadImageAsync(ImageSource imagesource, CancellationToken cancelationToken = default, float scale = 1)
+        {
+            if(imagesource is GravatarImageSource gis)
+            {
+                var imageBytes = await gis.GetGravatarAsync();
+
+                if (imageBytes.Length > 0)
+                {
+                    return UIImage.LoadFromData(NSData.FromArray(imageBytes));
+                }
+            }
+
+            return null;
+        }
+    }
+}
