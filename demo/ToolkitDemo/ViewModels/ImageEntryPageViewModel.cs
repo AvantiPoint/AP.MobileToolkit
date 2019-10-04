617227f565a22262531a3c35dda4318ffc663837
Rehan <rehan.siddiqui98@yahoo.com>
2019-10-04 15:45:08 +0500

    Issues #51 Added BorderlessEditor Added BorderlessPicker Added MaterialButton Added MaterialPicker Added CustomRadioButton Added ImageEntry Added Lowercase Converter

--- demo/ToolkitDemo/ViewModels/ImageEntryPageViewModel.cs
+++ demo/ToolkitDemo/ViewModels/ImageEntryPageViewModel.cs
@@ -1,14 +1,31 @@
-﻿using Prism.Logging;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AP.CrossPlatform.Collections;
+using Prism.Logging;
 using Prism.Navigation;
 using Prism.Services;
+using ToolkitDemo.Models;
+using ToolkitDemo.SideMenu;
 
 namespace ToolkitDemo.ViewModels
 {
     public class ImageEntryPageViewModel : DemoPageViewModelBase
     {
-        public ImageEntryPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService, ILogger logger)
+        public ObservableCollection<Grouping<Models.Category, Item>> Categories { get; set; }
+        public ObservableCollection<Item> Items;
+
+        public IMenu Menu { get; set; }
+        public ImageEntryPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService, ILogger logger, IMenu menu)
             : base(navigationService, pageDialogService, logger)
         {
+            Menu = menu;
+            Categories = Menu.Categories;
+
+            Items = new ObservableCollection<Item>();
+            foreach (var item in Menu.MenuItems)
+            {
+                Items.Add(item);
+            }
         }
     }
 }
