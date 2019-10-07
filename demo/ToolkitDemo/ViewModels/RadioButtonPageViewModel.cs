ba4e9ba19900aa33c143f2b7503085465daca806
Rehan <rehan.siddiqui98@yahoo.com>
2019-10-07 15:47:23 +0500

    Issue #51 Align Ids of SideMenu items Added AP.MobileToolkit reference to ToolkitDemo.Adroid and iOS Fixed Radio button screen Added list of items in RadioButtonPageViewModel and bind it to RadioButtonGroup Fixed image entry drawable ResourceId=0 exception Change android app icon

--- demo/ToolkitDemo/ViewModels/RadioButtonPageViewModel.cs
+++ demo/ToolkitDemo/ViewModels/RadioButtonPageViewModel.cs
@@ -1,14 +1,26 @@
-﻿using Prism.Logging;
+﻿using System.Collections.ObjectModel;
+using AP.CrossPlatform.Collections;
+using Prism.Logging;
 using Prism.Navigation;
 using Prism.Services;
+using ToolkitDemo.Models;
+using ToolkitDemo.SideMenu;
 
 namespace ToolkitDemo.ViewModels
 {
     public class RadioButtonPageViewModel : DemoPageViewModelBase
     {
-        public RadioButtonPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService, ILogger logger)
+        public IMenu Menu { get; set; }
+        public ObservableCollection<string> RadioItems { get; set; }
+        public RadioButtonPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService, ILogger logger, IMenu menu)
             : base(navigationService, pageDialogService, logger)
         {
+            RadioItems = new ObservableCollection<string>()
+            {
+                "Radio Option 1",
+                "Radio Option 2",
+                "Radio Option 3"
+            };
         }
     }
 }
