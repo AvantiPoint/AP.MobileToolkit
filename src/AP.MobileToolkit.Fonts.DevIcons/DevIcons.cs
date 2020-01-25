61855883185d954080e72b4cabeded7df5930803
Dan Siegel <me@dansiegel.net>
2020-01-25 14:24:09 -0800

    adding Material and Dev Icons

---
+++ src/AP.MobileToolkit.Fonts.DevIcons/DevIcons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+[assembly: ExportFont(AP.MobileToolkit.Fonts.DevIcons.FontName)]
+namespace AP.MobileToolkit.Fonts
+{
+    public static class DevIcons
+    {
+        internal const string FontName = "devicons.ttf";
+
+        public const string Prefix = "devicons";
+
+        public const string Version = "1.8.0";
+
+        public static readonly IconFont Font = new IconFont(FontName, Prefix, typeof(Mappings.DevIcons));
+    }
+}
---
+++ src/AP.MobileToolkit.Fonts.MaterialIcons/MaterialIcons.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+[assembly: ExportFont(AP.MobileToolkit.Fonts.MaterialIcons.FontName)]
+namespace AP.MobileToolkit.Fonts
+{
+    public static class MaterialIcons
+    {
+        internal const string FontName = "MaterialIcons-Regular.ttf";
+
+        public const string Prefix = "material";
+
+        public const string Version = "3.0.1";
+
+        public static readonly IconFont Font = new IconFont(FontName, Prefix, typeof(Mappings.MaterialIcons));
+    }
+}
