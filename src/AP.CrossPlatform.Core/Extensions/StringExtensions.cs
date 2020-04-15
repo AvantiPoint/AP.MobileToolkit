c1d83f10e6773a12a511e3f120341c8b8ad36320
Dan Siegel <me@dansiegel.net>
2020-04-15 08:31:54 -0700

    add ascii conversion helpers

---
+++ src/AP.CrossPlatform.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace AP.CrossPlatform.Extensions
+{
+    public static class StringExtensions
+    {
+        private static readonly Dictionary<char, string> AsciiConversions = new Dictionary<char, string>
+        {
+            { '’', "'" },
+            { '‘', "'" },
+            { '“', "\"" },
+            { '”', "\"" },
+            { '"', "\"" },
+            { '®', "(R)" },
+            { '©', "(C)" },
+            { '«', "<<" },
+            { '»', ">>" },
+            { '±', "+/-" },
+            { '¢', "c" },
+            { '£', "L" },
+            { '…', "..." },
+            { '•', "*" },
+            { '⁄', "/" },
+            { '™', "TM" },
+            { '←', "<--" },
+            { '→', "-->" },
+            { '↔', "<-->" },
+            { '⇐', "<==" },
+            { '⇒', "==>" },
+            { '⇔', "<==>" },
+            { '−', "-" },
+            { '∗', "*" },
+            { '∼', "~" },
+            { '≅', "~=" },
+            { '≈', "~" },
+            { '≠', "!=" },
+            { '≤', "<=" },
+            { '≥', ">=" },
+            { '⟨', "<" },
+            { '⟩', ">" },
+            { '◊', "<>" },
+            { '⊕', "(+)" },
+            { '⊗', "(x)" },
+            { '¼', "1/4" },
+            { '½', "1/2" },
+            { '¾', "3/4" }
+        };
+
+        public static string ToASCII(this string str)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                var c = str[i];
+                if ((int)c >= 128)
+                {
+                    var ascii = AsciiConversions.ContainsKey(c) ? AsciiConversions[c] : "?";
+                    sb.Append(ascii);
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool HasNonASCIIChars(this string str)
+        {
+            foreach (var c in str)
+            {
+                bool isAscii = c < 128;
+                if (!isAscii)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/AP.CrossPlatform.Core.Tests/Fixtures/Extensions/StringExtensionFixture.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AP.CrossPlatform.Extensions;
+using Xunit;
+
+namespace AP.CrossPlatform.Core.Tests.Fixtures.Extensions
+{
+    public class StringExtensionFixture
+    {
+        private const string DoubleQuoteInput = "“Hello World”";
+        private const string SingleQuoteInput = "‘Hello World’";
+
+        [Fact]
+        public void ReplacesDoubleQuotes()
+        {
+            Assert.True(DoubleQuoteInput.HasNonASCIIChars());
+
+            var ascii = DoubleQuoteInput.ToASCII();
+            Assert.NotEqual(DoubleQuoteInput, ascii);
+
+            Assert.False(ascii.HasNonASCIIChars());
+            Assert.Equal(DoubleQuoteInput.Length, ascii.Length);
+            Assert.Equal("\"Hello World\"", ascii);
+        }
+
+        [Fact]
+        public void ReplacesSingleQuotes()
+        {
+            Assert.True(SingleQuoteInput.HasNonASCIIChars());
+
+            var ascii = SingleQuoteInput.ToASCII();
+            Assert.NotEqual(SingleQuoteInput, ascii);
+
+            Assert.False(ascii.HasNonASCIIChars());
+            Assert.Equal(SingleQuoteInput.Length, ascii.Length);
+            Assert.Equal("'Hello World'", ascii);
+        }
+
+        [Fact]
+        public void ReplacesCommonFraction()
+        {
+            var input = "¾";
+            Assert.True(input.HasNonASCIIChars());
+
+            var ascii = input.ToASCII();
+
+            Assert.False(ascii.HasNonASCIIChars());
+            Assert.Equal(3, ascii.Length);
+
+            Assert.Equal("3/4", ascii);
+        }
+
+        [Fact]
+        public void ReplacesCopyrightSymbol()
+        {
+            var input = "© AvantiPoint";
+            Assert.True(input.HasNonASCIIChars());
+
+            var ascii = input.ToASCII();
+
+            Assert.False(ascii.HasNonASCIIChars());
+
+            Assert.Equal("(C) AvantiPoint", ascii);
+        }
+    }
+}
