d5da9b9852efa9a3f9eff81ed7d9d15626200d66
Dan Siegel <me@dansiegel.net>
2020-06-04 15:01:40 -0700

    demo updates

---
+++ demo/ToolkitDemo/Views/GravatarPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ToolkitDemo.Helpers;
+using ToolkitDemo.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: MenuItem("Gravatar Image Source", nameof(GravatarPage), MenuGroup.Controls)]
+namespace ToolkitDemo.Views
+{
+    public partial class GravatarPage
+    {
+        public GravatarPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
