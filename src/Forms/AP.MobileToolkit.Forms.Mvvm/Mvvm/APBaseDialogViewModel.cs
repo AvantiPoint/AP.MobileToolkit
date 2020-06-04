ebcdae91b5d73612e195ee0667433525bd794e00
Dan Siegel <me@dansiegel.net>
2020-06-04 09:23:22 -0700

    Rx-ify and provide DismissCommand

--- src/Forms/AP.MobileToolkit.Forms.Mvvm/Mvvm/APBaseDialogViewModel.cs
+++ src/Forms/AP.MobileToolkit.Forms.Mvvm/Mvvm/APBaseDialogViewModel.cs
@@ -1,23 +1,37 @@
 ﻿using System;
+using System.Reactive;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Prism.AppModel;
 using Prism.Navigation;
 using Prism.Services.Dialogs;
+using ReactiveUI;
 
 namespace AP.MobileToolkit.Mvvm
 {
-    public abstract class APBaseDialogViewModel : IAutoInitialize, IDialogAware, IDestructible
+    public abstract class APBaseDialogViewModel : ReactiveObject, IAutoInitialize, IDialogAware, IDestructible
     {
         protected CompositeDisposable Disposables { get; private set; }
 
         protected APBaseDialogViewModel()
         {
             Disposables = new CompositeDisposable();
+            DismissCommand = ReactiveCommand.Create(DismissCommandExecuted, this.WhenAnyValue(x => x.CanClose))
+                .DisposeWith(Disposables);
         }
 
         public event Action<IDialogParameters> RequestClose;
 
-        public virtual bool CanCloseDialog() => true;
+        private bool _canClose;
+        public bool CanClose
+        {
+            get => _canClose;
+            set => this.RaiseAndSetIfChanged(ref _canClose, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> DismissCommand { get; }
+
+        public bool CanCloseDialog() => CanClose;
 
         public virtual void OnDialogClosed()
         {
@@ -40,5 +54,8 @@ namespace AP.MobileToolkit.Mvvm
             Disposables.Dispose();
             Disposables = null;
         }
+
+        private void DismissCommandExecuted() =>
+            RaiseRequestClose();
     }
 }
